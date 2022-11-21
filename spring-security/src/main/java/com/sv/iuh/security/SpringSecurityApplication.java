423cb10d3bd6678bb4e84c8973b79bbcd884cf49
mhxx307 <minhquan.lavo@gmail.com>
2022-11-21 16:38:48 +0700

    #3-finish auth service

---
+++ spring-security/src/main/java/com/sv/iuh/security/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.sv.iuh.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
